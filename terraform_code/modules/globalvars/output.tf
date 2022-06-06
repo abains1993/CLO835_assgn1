e818c1ee8f0d149d1937afb0226ad40bf523499f
abains1993 <arunbains2015@gmail.com>
2022-06-06 01:43:35 +0000

    added terraform code

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Arun-Bains"
+    "App"   = "Web"
+    "Project" = "CLO835-assgn1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
